56bc808c9bae335a400a3fee79249425258ef04a
parthravalgit <59275745+parthravalgit@users.noreply.github.com>
2021-07-31 13:03:10 +0530

    Update Main.tf

--- Main.tf
+++ Main.tf
@@ -6,15 +6,62 @@ terraform {
   backend "s3" {
     bucket = "firstbackendtf"
     key    = "tfbackend_1.tfstate"
-    region = "us-east-1"
+    region = "us-east-2"
   }
 }
 
-## random provider
-provider "random" {}
-
 ## Provider us-east-1
 provider "aws" {
-  region = "us-east-1"
+  region     = "us-west-2"  
+  alias = "uswest2"
+}
+
+resource "aws_instance" "us-west2" {
+  ami           = "ami-005e54dee72cc1d00" # us-west-2
+  instance_type = "t2.micro"
+  provider = aws.uswest2
+}
+
+resource "aws_s3_bucket" "mybucket" {
+  bucket = "my-s3-tf-test-bucket"
+  acl    = "private"
+
+  tags = {
+    Name        = "My terraform bucket"
+    Environment = "Dev-Env"
+  }
+  versioning {
+    enabled = true
+  }
+}
+
+resource "aws_vpc" "dev" {
+  cidr_block       = "10.0.0.0/16"
+  instance_tenancy = "default"
+
+  tags = {
+    Name = "dev-vpc"
+  }
+}
+
+resource "aws_subnet" "sub" {
+  vpc_id     = aws_vpc.dev.id
+  cidr_block = "10.0.1.0/24"
+
+  tags = {
+    Name = "dev-subnet"
+  }
+}
+
+resource "aws_db_instance" "projectdb" {
+  allocated_storage    = 10
+  engine               = "mysql"
+  engine_version       = "8.0.19"
+  instance_class       = "db.t3.micro"
+  name                 = "myterraformdb"
+  username             = "foo"
+  password             = "foobarbaz"
+  parameter_group_name = "default.mysql8.0"
+  skip_final_snapshot  = true
 }
 
